084de31925189a68257edc4257c47c8cc0c95c34
BannyT <tumusiimebarnabus@gmail.com>
2025-06-28 14:42:17 +0300

    updates data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./Header"
 import Carousel from './Carousel';
+import TopProperties from './TopProperties';
 
 function App() {
   return (
     <div className="App">
         <Header/> 
         <Carousel/>
+        <TopProperties/>
     </div>
   );
 }
---
+++ src/TopProperties.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import "./TopProperties.css"
+
+const TopProperties = () => {
+    const properties = [
+        {
+          id: 1,
+          title: 'Oceanfront Villa',
+          location: 'Malibu, California',
+          price: '$5,200/month',
+          image: 'https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        },
+        {
+          id: 2,
+          title: 'Mountain Retreat',
+          location: 'Aspen, Colorado',
+          price: '$3,400/month',
+          image: 'https://images.pexels.com/photos/2102587/pexels-photo-2102587.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        },
+        {
+          id: 3,
+          title: 'City Loft',
+          location: 'New York, NY',
+          price: '$4,800/month',
+          image: 'https://images.pexels.com/photos/439391/pexels-photo-439391.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        },
+        {
+          id: 4,
+          title: 'Santorini Escape',
+          location: 'Santorini, Greece',
+          price: '$4,200/month',
+          image: 'https://images.pexels.com/photos/169677/pexels-photo-169677.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        },
+      ];
+    
+
+  return (
+       <section className="top-properties">
+           <h2>Top Properties in Us</h2>
+         <input className='search' type="text" placeholder='Search property by name' /> 
+         <div className="property-grid">
+        {properties.map((prop) => (
+          <div className="property-card" key={prop.id}>
+            <img src={prop.image} alt={prop.title} />
+            <div className="property-details">
+              <h3>{prop.title}</h3>
+              <p>{prop.location}</p>
+              <span>{prop.price}</span>
+              <button  className="book-now">Book Now</button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+       </section>
+      
+  )
+}
+
+export default TopProperties
