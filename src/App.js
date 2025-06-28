1952f0319c1b4cf697b0728f51b6eeb25e341d68
BannyT <tumusiimebarnabus@gmail.com>
2025-06-28 12:56:05 +0300

    added array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./Header"
+import Carousel from './Carousel';
 
 function App() {
   return (
     <div className="App">
         <Header/> 
-        
+        <Carousel/>
     </div>
   );
 }
---
+++ src/Carousel.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./Carousel.css"
+
+const Carousel = () => {
+    
+    const slides = [
+        {
+          id: 1,
+          image: 'https://images.unsplash.com/photo-1572120360610-d971b9d7767c?auto=format&fit=crop&w=1600&q=80',
+          title: 'Luxury Villa in Los Angeles',
+          subtitle: '$4,200/month - 5 Bed, 4 Bath',
+        },
+        {
+          id: 6,
+          image: 'https://images.pexels.com/photos/1643383/pexels-photo-1643383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+          title: 'Modern Beachfront Villa',
+          subtitle: '$5,000/month – Ocean View & Infinity Pool',
+        },
+        {
+          id: 3,
+          image: 'https://images.unsplash.com/photo-1507089947368-19c1da9775ae?auto=format&fit=crop&w=1600&q=80',
+          title: 'Modern Apartment in New York',
+          subtitle: '$2,200/month - Central Park View',
+        },
+      ];
+
+  return (
+    <div> Here is my carousel</div>
+  )
+}
+
+export default Carousel
