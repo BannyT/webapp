574f50e9140e2b228da45664bc4f2224ecc0d546
BannyT <tumusiimebarnabus@gmail.com>
2025-07-05 13:46:35 +0300

    updates logn

--- src/Login.js
+++ src/Login.js
@@ -3,9 +3,20 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-         <h1>Login is here </h1>
-    </div>
+     <section className='login-section'>
+         <div className='login-card'>
+            <h2>Welcome Back To Login</h2>
+            <p>log in to your application  here</p>
+            <form  className='login-form'>
+                 <input required type='email' placeholder='Enter your Email' />
+                 <input required type='password' placeholder='Enter your Password' />
+                 <button>Login</button>
+            </form>
+            <p className='login-footer'>
+                Dont have an Account? <a href='/signup'> SignUp</a>
+            </p>
+         </div>
+     </section>
   )
 }
 
