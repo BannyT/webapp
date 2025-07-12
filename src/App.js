bca772afcea16dd1f5d496c73c0547d3476f518d
BannyT <tumusiimebarnabus@gmail.com>
2025-07-12 13:21:15 +0300

    updates about

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import { Routes,Route, BrowserRouter } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
  import Signup  from  "./Signup"
+import About from './About';
 
 function App() {
   return (
     <BrowserRouter>
        <Header/>
        <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/> 
+        <Route path="/about" element={<About/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>  
         <Route path="/properties" element={<TopProperties/>}/> 
