be9b0d296a365fa40a06b3523a127db168301ba4
BannyT <tumusiimebarnabus@gmail.com>
2025-07-19 12:43:02 +0300

    updates data

--- src/Signup.js
+++ src/Signup.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./Signup.css"
 
 const Signup = () => {
+  const [email, setEmail]= useState("")
+  const [password,setPassword]=useState("")
+
+  console.log(password)
   return (
        <section className='signup-section'>
            <div className='signup-card'>
@@ -9,8 +13,8 @@ const Signup = () => {
               <h2>Create Account</h2>
               <p> Join us and explore amazing properties</p>
               <form className='signup-form'>
-                     <input type="email"placeholder='Enter Your Email' />
-                     <input type="password"placeholder='Enter Your Password' />
+                     <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email"placeholder='Enter Your Email' />
+                     <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" placeholder='Enter Your Password' />
                      <button>Create Account</button>
               </form>
               <p>Already have an Account? <a href='/login'>Login</a></p>
