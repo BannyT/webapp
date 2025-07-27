08366dd238e84ed531b547665d73f46d2124f1fb
BannyT <tumusiimebarnabus@gmail.com>
2025-07-27 11:32:28 +0300

    updates data

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,16 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
+import { getAuth } from 'firebase/auth'
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const auth= getAuth()
+  const user= auth.currentUser;
 
+  const signOut =()=>{
+     auth.signOut()
+     alert('Logged out')
+  }
   return (
     
     <header className="navbar">
@@ -19,7 +26,8 @@ const Header = () => {
         <Link to="/contact">Contact</Link> 
         {/* <Link to="/blog">Blog</Link> */}
         <div className="navbar__auth">
-          <Link to="/login" className="btn login">Login</Link>
+          {user?<><button onClick={signOut} className="btn login">Logout</button></>: 
+          <><Link to="/login" className="btn login">Login</Link></>}
           <Link to="/signup" className="btn signup">Sign Up</Link>
           <Link to='/mybooking' className='btn login'>My booking</Link>
         </div>
--- src/TopProperties.js
+++ src/TopProperties.js
@@ -4,10 +4,14 @@ import Footer from './Footer';
 import "./TopProperties.css"
 import { properties } from './PropertyData';
 
+import { getAuth } from 'firebase/auth'
 const TopProperties = () => {
-    const nav = useNavigate();
+    const nav = useNavigate();   
+    const auth= getAuth()
+    const user= auth.currentUser;
     const [query,setQuerry]=useState('')
 
+     console.log(user)
    
     
       //javascrio code 
@@ -28,6 +32,7 @@ const TopProperties = () => {
        <section className="top-properties">
            <h2>Top Properties in Us</h2>
          <input value={query} onChange={(e)=>setQuerry(e.target.value)} className='search' type="text" placeholder='Search property by name or location' /> 
+          {user?<>    
          <div className="property-grid">
          {filteredproperties.map((prop) => (
           <div className="property-card" key={prop.id}>
@@ -40,7 +45,27 @@ const TopProperties = () => {
             </div>
           </div>
         ))}
+          
+        {!user? 
+          <p className='book-now'>Login to view more properties</p>:''}
          </div><br></br><br></br>
+          </> :<>  
+         <div className="property-grid">
+         {filteredproperties.slice(0,2).map((prop) => (
+          <div className="property-card" key={prop.id}>
+            <img className='propimage' src={prop.image} alt={prop.title} />
+            <div className="property-details">
+              <h3>{prop.title}</h3>
+              <p>{prop.location}</p>
+              <span>{prop.price}</span>
+              <button onClick={()=>moveToBooking(prop)} className="book-now">Book Now</button>
+            </div>
+          </div>
+        ))}
+        {!user? 
+          <p className='book-now'>Login to view more properties</p>:''}
+         </div><br></br><br></br>
+          </>}
         {/* <Footer/> */}
        </section>
       
