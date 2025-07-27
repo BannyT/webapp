7ee6de9f0a4ac29bc07603fc2ccc8c16c79b4aa0
BannyT <tumusiimebarnabus@gmail.com>
2025-07-27 10:59:46 +0300

    updates data

--- src/ContactUS.js
+++ src/ContactUS.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React,{useState, useRef} from 'react'
 import "./ContactUs.css"
+import emailjs from '@emailjs/browser';
 
 const ContactUS = () => {
+    const [name,setName]=useState('')
+    const [email, setEmail]=useState('')
+    const [message,setMessage]=useState('')
+    const form = useRef();
+
+    //send message function
+
+    const sendMessage =(e)=>{
+        e.preventDefault()
+        emailjs.sendForm(
+            'service_btccnve',      // Replace with EmailJS service ID
+            'template_n3ldxt2',     // Replace with EmailJS template ID
+            form.current,
+            'u-MjiBbvb2IsQAiYL'       // Replace with EmailJS public key
+          )
+          .then((result) => {
+            alert('Message sent successfully!');
+            console.log(result.text);
+          }, (error) => {
+            alert('Failed to send message.');
+            console.log(error.text);
+          });
+
+    }
+
   return (
      <section  className="contact-wrapper">
            <div className='contact-card'>
@@ -14,14 +40,14 @@ const ContactUS = () => {
                         <p><strong>Location:</strong> Akright Bwebajja, Entebbe</p>
                     </div>
                </div>
-               <form className='contact-form'>
-                    <input type="text" placeholder='Enter Full NAme' />
+               <form  ref={form} onSubmit={sendMessage} className='contact-form'>
+                    <input name='name' value={name} onChange={(e)=>setName(e.target.value)} type="text" placeholder='Enter Full NAme' />
                     
-                    <input type="Email" placeholder='Enter Email Address' />
-                    <textarea rows="5" placeholder='Enter your MEssage'>
+                    <input name='email' value={email} onChange={(e)=>setEmail(e.target.value)} type="Email" placeholder='Enter Email Address' />
+                    <textarea name='message'  value={message} onChange={(e)=>setMessage(e.target.value)} rows="5" placeholder='Enter your MEssage'>
 
                     </textarea>
-                    <button>Send Message</button>
+                    <button  type="submit">Send Message</button>
                </form>
 
            </div>
